1f18cdb03cb737684852ea28f995b65b1d101812
erncn01 <erencan1190@hotmail.com>
2024-10-25 18:04:28 +0300

    initial commit

---
+++ e/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  e
+//
+//  Created by Erencan Malagic on 20.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var first: UITextField!
+    
+    
+    @IBOutlet weak var second: UITextField!
+    
+    
+    
+    @IBOutlet weak var label: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    
+    }
+    
+    
+
+
